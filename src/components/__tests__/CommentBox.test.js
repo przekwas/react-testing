b7c094873a06d2ccfd20d86166039ee8fa923dab
Luke Przekwas <galactica@The-Galactica.local>
2018-07-18 17:13:30 -0500

    adding simulate change objects

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,3 +18,21 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('has a form submit that empties the textarea', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
