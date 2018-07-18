7eb40674640b844df0703aea3cce97198c1bc02f
Luke Przekwas <galactica@The-Galactica.local>
2018-07-18 16:22:05 -0500

    adding beforeEach construct

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+//runs before each test
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
 
 //test that app component shows a comment box
 it('shows a comment box', () => {
-    //render react component but none of its children
-    const wrapped = shallow(<App />);
-
-    //expect 1 CommentBox inside app
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 //test that app component shows a comment list
 it('shows a comment list', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
